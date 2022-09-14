b4a0a3cbbda15cece346682e79bcde5b9e28a984
Alex | MrMinemeet <alexander.voglsperger+github@proton.me>
2022-09-14 15:10:08 +0200

    Refactored stats.go
    Instead of passing a new string when filtering the data, the string to filter are defined as constants at the top now

--- stats.go
+++ stats.go
@@ -7,6 +7,13 @@ import (
 	"strings"
 )
 
+const FarmStatus = "Farming status:"
+const PlotCount = "Plot count for all harvesters:"
+const ExpectedTimeToWin = "Expected time to win:"
+const EstimatedNetspace = "Estimated network space:"
+const CurrentDifficulty = "Current difficulty:"
+const Network = "Network:"
+
 type TStats struct {
 	Ettw       string // Expected time to win
 	Netspace   string
@@ -38,32 +45,32 @@ func RefreshStats() (TStats, error) {
 
 	// Get information from cmd output
 	for _, line := range output {
-		if strings.Contains(line, "Plot count for all harvesters:") && stats.PlotCount == 0 {
+		if strings.Contains(line, PlotCount) && stats.PlotCount == 0 {
 			// Plot Count
-			plotCount, err := strconv.Atoi(strings.Trim(strings.TrimPrefix(line, "Plot count for all harvesters:"), " "))
+			plotCount, err := strconv.Atoi(strings.Trim(strings.TrimPrefix(line, PlotCount), " "))
 			if err != nil {
 				stats.PlotCount = -1
 			} else {
 				stats.PlotCount = plotCount
 			}
-		} else if strings.Contains(line, "Farming status:") && strings.EqualFold(stats.FarmStatus, "") {
+		} else if strings.Contains(line, FarmStatus) && strings.EqualFold(stats.FarmStatus, "") {
 			// Farming Status
-			stats.FarmStatus = strings.TrimPrefix(line, "Farming status:")
-		} else if strings.Contains(line, "Expected time to win:") && strings.EqualFold(stats.Ettw, "") {
+			stats.FarmStatus = strings.TrimPrefix(line, FarmStatus)
+		} else if strings.Contains(line, ExpectedTimeToWin) && strings.EqualFold(stats.Ettw, "") {
 			// Expected time to win
-			stats.Ettw = strings.TrimPrefix(line, "Expected time to win:")
-		} else if strings.Contains(line, "Estimated network space:") && strings.EqualFold(stats.Netspace, "") {
+			stats.Ettw = strings.TrimPrefix(line, ExpectedTimeToWin)
+		} else if strings.Contains(line, EstimatedNetspace) && strings.EqualFold(stats.Netspace, "") {
 			// Netspace
-			stats.Netspace = strings.TrimPrefix(line, "Estimated network space:")
-		} else if strings.Contains(line, "Current difficulty:") && stats.Difficulty == 0 {
+			stats.Netspace = strings.TrimPrefix(line, EstimatedNetspace)
+		} else if strings.Contains(line, CurrentDifficulty) && stats.Difficulty == 0 {
 			// Difficulty
-			difficulty, err := strconv.Atoi(strings.Trim(strings.TrimPrefix(line, "Current difficulty:"), " "))
+			difficulty, err := strconv.Atoi(strings.Trim(strings.TrimPrefix(line, CurrentDifficulty), " "))
 			if err != nil {
 				stats.Difficulty = -1
 			} else {
 				stats.Difficulty = difficulty
 			}
-		} else if strings.Contains(line, "Network:") && strings.EqualFold(stats.Network, "") {
+		} else if strings.Contains(line, Network) && strings.EqualFold(stats.Network, "") {
 			// Network
 			tmp := strings.Split(line, " ")
 			stats.Network = tmp[1]
